a463abbe79f758aef886be552cd431aca612c02d
Stephan Lüscher <stephan.luescher@mykolab.com>
2024-06-28 08:44:30 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 ## DevPod dotfile install
 if [ "${DEVPOD}" == "true" ]; then
+	echo "preparing environment"
 	export XDG_CONFIG_HOME="${HOME}/.config"
 	export PATH="${PATH}":"${HOME}/.local/bin"
 	mkdir -p "${XDG_CONFIG_HOME}"
+	echo "installing chezmoi"
 	sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "${HOME}/.local/bin"
 	chezmoi init https://github.com/tepene/dotfiles.git
+	echo "applying configurations"
 	chezmoi apply --force "${XDG_CONFIG_HOME}/nvim"
 fi
